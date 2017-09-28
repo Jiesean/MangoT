b3151d22de29a8e2f394cd6e8635b5ff072c0f84
Jiesean <out_frozen@163.com>
2017-09-28 23:30:10 +0800

    Add SettingWindow

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -2,12 +2,16 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame implements ActionListener{
     private Dimension screenSize;
     private StringBuffer keywordString =new StringBuffer();
+
+    private JButton settingBtn;
 
     public MainWindow(){
         screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -20,71 +24,62 @@ public class MainWindow extends JFrame{
         JPanel projectPanel = new JPanel();
         projectPanel.setBackground(Color.YELLOW);
         JPanel editorPanel = new JPanel();
-        JPanel oututPanel = new JPanel();
-        oututPanel.setBackground(Color.BLUE);
-
-
+        JPanel outputPanel = new JPanel();
+        outputPanel.setBackground(Color.BLUE);
 
 
         // toolbaranel
         JButton runBtn = new JButton("Run");
+        runBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("runBtn clicked");
+            }
+        });
         JButton debugBtn = new JButton("Debug");
         JButton openBtn = new JButton("Open");
         JButton saveBtn = new JButton("Save");
         JButton stopBtn = new JButton("Stop");
+        JButton settingBtn = new JButton("Setting");
 
         toolbarPanel.add(openBtn);
         toolbarPanel.add(saveBtn);
         toolbarPanel.add(runBtn);
         toolbarPanel.add(debugBtn);
         toolbarPanel.add(stopBtn);
-
-
+        toolbarPanel.add(settingBtn);
 
         // editorPanel
-        MyTextPane textPane = new MyTextPane();
-        textPane.setBackground(Color.PINK);
-        textPane.setMinimumSize(new Dimension((int)(screenSize.getWidth()/3),(int)(screenSize.getHeight()/3)));
-        textPane.setEditable(true);
-//        textPane.addKeyListener(new KeyListener() {
-//            @Override
-//            public void keyTyped(KeyEvent e) {
-//
-//            }
-//
-//            @Override
-//            public void keyPressed(KeyEvent e) {
-//                if (e.getKeyCode() == KeyEvent.VK_0) {
-//                    System.out.println("pressed 0");
-//                    System.out.println(keywordString.toString());
-//                    keywordString.delete(0,keywordString.length());
-//
-//                }else{
-//                    keywordString.append(e.getKeyChar());
-//                }
-//            }
-//
-//            @Override
-//            public void keyReleased(KeyEvent e) {
-//
-//            }
-//        });
+        CodeEditor codeEditor = new CodeEditor();
+        codeEditor.setBackground(Color.PINK);
+//        codeEditor.setMinimumSize(new Dimension((int)(screenSize.getWidth()/3),(int)(screenSize.getHeight()/3)));
+        codeEditor.setEditable(true);
 
-
-        JScrollPane js=new JScrollPane(textPane);
+        JScrollPane js=new JScrollPane(codeEditor);
         editorPanel.setLayout(new BorderLayout());
         editorPanel.add(js);
 
+        JTextPane outputPane = new JTextPane();
+        JScrollPane outputScrollPane=new JScrollPane(outputPane);
+        outputPanel.setMinimumSize(new Dimension((int)screenSize.getWidth()/2,(int)(screenSize.getHeight()/4)));
+        outputPanel.setLayout(null);
+        outputPanel.add(outputScrollPane);
+
 
         getContentPane().add(toolbarPanel,BorderLayout.NORTH);
-        getContentPane().add(oututPanel,BorderLayout.SOUTH);
+        getContentPane().add(outputPanel,BorderLayout.SOUTH);
         getContentPane().add(projectPanel,BorderLayout.WEST);
         getContentPane().add(editorPanel,BorderLayout.CENTER);
 
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(settingBtn)) {
 
+        }
     }
 }
---
+++ src/gui/SettingWindow.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SettingWindow extends JFrame implements ActionListener{
+
+    public SettingWindow(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
